cd73c9dcb7ef66ddc17b239f438d55c879dc7ae2
Chsaleem31 <devsaleemch@gmail.com>
2024-01-16 09:22:32 -0800

    added user otp confirmation

--- accounts/schema.py
+++ accounts/schema.py
@@ -2,7 +2,6 @@ import graphene
 import boto3
 from botocore.exceptions import ClientError
 
-# AWS Cognito settings (replace with your values)
 COGNITO_USER_POOL_ID = 'eu-north-1_6Yucu5ItA'
 COGNITO_CLIENT_ID = '5g7inc6rbu678beipjgq74fjod'
 COGNITO_REGION = 'eu-north-1'
@@ -16,7 +15,8 @@ class UserType(graphene.ObjectType):
     name = graphene.String()
     username = graphene.String()
     email = graphene.String()
-    # Add other user attributes as needed
+    height = graphene.String()
+    weight = graphene.String()
 
 # Queries
 class Query(graphene.ObjectType):
@@ -37,22 +37,47 @@ class Query(graphene.ObjectType):
 class SignupMutation(graphene.Mutation):
     class Arguments:
         username = graphene.String(required=True)
+        name = graphene.String(required=True)
         password = graphene.String(required=True)
         email = graphene.String(required=True)
+        height = graphene.String(required=True)
+        weight = graphene.String(required=True)
 
     user = graphene.Field(UserType)
 
-    def mutate(self, info, username, password, email):
+    def mutate(self, info, username, name, password, email, height, weight):
         try:
             cognito_client.sign_up(
                 ClientId=COGNITO_CLIENT_ID,
                 Username=username,
                 Password=password,
                 UserAttributes=[
-                    {'Name': 'email', 'Value': email}
+                    {'Name': 'email', 'Value': email},
+                    {'Name': 'name', 'Value': name},
+                    {'Name': 'custom:height', 'Value': height},
+                    {'Name': 'custom:weight', 'Value': weight}
                 ]
             )
             return SignupMutation(user=UserType(username=username, email=email))
+        except ClientError as error:
+            raise Exception(str(error))
+        
+class UserConfirmationMutation(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+        otp = graphene.String(required=True)
+
+    success = graphene.Boolean()
+
+    def mutate(self, info, username, otp):
+        try:
+            cognito_client.confirm_sign_up(
+                ClientId=COGNITO_CLIENT_ID,
+                Username=username,
+                ConfirmationCode=otp
+            )
+            
+            return UserConfirmationMutation(success=True)
         except ClientError as error:
             raise Exception(str(error))
 
@@ -61,95 +86,34 @@ class SigninMutation(graphene.Mutation):
         username = graphene.String(required=True)
         password = graphene.String(required=True)
 
-    success = graphene.Boolean()
+    accessToken = graphene.String()
+    refreshToken = graphene.String()
+    userId = graphene.String()
+    userName = graphene.String()
+    userEmail = graphene.String()
 
     def mutate(self, info, username, password):
         try:
-            cognito_client.initiate_auth(
+            res = cognito_client.initiate_auth(
                 ClientId=COGNITO_CLIENT_ID,
                 AuthFlow='USER_PASSWORD_AUTH',
                 AuthParameters={'USERNAME': username, 'PASSWORD': password}
             )
-            return SigninMutation(success=True)
+            token = res['AuthenticationResult']['AccessToken']
+            rtoken = res['AuthenticationResult']['RefreshToken']
+            response = cognito_client.admin_get_user(
+                UserPoolId=COGNITO_USER_POOL_ID,
+                Username=username
+            )
+            user_attributes = {attr['Name']: attr['Value'] for attr in response['UserAttributes']}
+            
+            return SigninMutation(accessToken=token, refreshToken=rtoken, userId=user_attributes.get('sub'), userName=response['Username'], userEmail=user_attributes.get('email'))
         except ClientError as error:
             raise Exception(str(error))
-
-class UpdateUserMutation(graphene.Mutation):
-    class Arguments:
-        username = graphene.String(required=True)
-        email = graphene.String()
-
-    success = graphene.Boolean()
-
-    def mutate(self, info, username, email=None):
-        attributes = []
-        if email:
-            attributes.append({'Name': 'email', 'Value': email})
-
-        if attributes:
-            try:
-                cognito_client.admin_update_user_attributes(
-                    UserPoolId=COGNITO_USER_POOL_ID,
-                    Username=username,
-                    UserAttributes=attributes
-                )
-                return UpdateUserMutation(success=True)
-            except ClientError as error:
-                raise Exception(str(error))
-        else:
-            return UpdateUserMutation(success=False)
 
 class Mutation(graphene.ObjectType):
     signup = SignupMutation.Field()
     signin = SigninMutation.Field()
-    update_user = UpdateUserMutation.Field()
+    confirm_user = UserConfirmationMutation.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
-
-
-
-# import graphene
-# import requests
-
-# class MealType(graphene.ObjectType):
-#     userId = graphene.ID()
-#     date = graphene.String()
-#     name = graphene.String()
-#     calories = graphene.Int()
-#     MealId = graphene.ID()
-
-# class Query(graphene.ObjectType):
-#     list_meals = graphene.List(MealType, user_id=graphene.ID(required=True))
-
-#     def resolve_list_meals(self, info, user_id):
-#         auth_header = info.context.META.get('HTTP_AUTHORIZATION')
-#         if not auth_header:
-#             raise Exception("Authorization header is required")
-
-#         # AWS AppSync GraphQL endpoint and query
-#         graphql_endpoint = 'https://jvztlvvvhraivddtrhy3ul27iq.appsync-api.eu-north-1.amazonaws.com/graphql'
-#         graphql_query = f'''
-#         query ListMeals {{
-#             listMeals(userId: "{user_id}") {{
-#                 userId
-#                 date
-#                 name
-#                 calories
-#                 MealId
-#             }}
-#         }}
-#         '''
-
-#         # Headers including the authorization token
-#         headers = {
-#             'Content-Type': 'application/json',
-#             'Authorization': auth_header  # Replace with the appropriate token
-#         }
-
-#         # Request to AWS AppSync
-#         response = requests.post(graphql_endpoint, json={'query': graphql_query}, headers=headers)
-#         meals_data = response.json().get('data', {}).get('listMeals', [])
-#         return [MealType(**meal) for meal in meals_data]
-
-# # Define the schema
-# schema = graphene.Schema(query=Query)
